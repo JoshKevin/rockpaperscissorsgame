03e051d9ee195f5150174c34c8e8e832f8a5b8f0
JoshKevin <joshkevinserdan@gmail.com>
2023-01-23 02:35:15 +0800

    make randomized selection for computer

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 let playerSelection = prompt("Do you choose rock, paper or scissors?");
 console.log("Player chose " + playerSelection);
+
+let array = ['rock', 'paper', 'scissors'];
+
+function getComputerSelection(arr) {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    const item = arr[randomIndex];
+    console.log("Computer chose " + item);
+    return item;
+}
+
+const computerSelection = getComputerSelection(array);
