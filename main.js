3bc993601ee9d1ff6ae8a985a936f30a81776fd0
JoshKevin <joshkevinserdan@gmail.com>
2023-02-09 23:47:58 +0800

    make sstart game button appear choice buttons and result pane

--- main.js
+++ main.js
@@ -1,25 +1,8 @@
-let playerSelection = prompt("Do you choose rock, paper or scissors?");
-console.log("Player chose " + playerSelection);
-
-let array = ['rock', 'paper', 'scissors'];
-
-function getComputerSelection(arr) {
-    const randomIndex = Math.floor(Math.random() * arr.length);
-    const item = arr[randomIndex];
-    console.log("Computer chose " + item);
-    return item;
-}
-
-const computerSelection = getComputerSelection(array);
-
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === "rock" && computerSelection === "paper") console.log("You Lose!");
-    else if (playerSelection === "rock" && computerSelection === "scissors") console.log("You win!");
-    else if (playerSelection === "scissors" && computerSelection === "rock") console.log("You Lose");
-    else if (playerSelection === "scissors" && computerSelection === "paper") console.log("You win!");
-    else if (playerSelection === "paper" && computerSelection === "scissors") console.log("You Lose");
-    else if (playerSelection === "paper" && computerSelection === "rock") console.log("You win!");
-}
-
-playRound(playerSelection, computerSelection);
-
+const startbtn = document.querySelector('#startbtn');
+const hidden = document.querySelectorAll('.hidden');
+startbtn.addEventListener('click', () => {
+    startbtn.style.display = 'none';
+    hidden.forEach((hidden) => {
+        hidden.style.display = 'block';
+    });
+});
